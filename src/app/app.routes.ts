6439033bd0645479955d16a79e1ef60de0862185
Gus Ariotti <gustavo.ariotti@gmail.com>
2025-10-24 19:34:22 -0300

    feat: routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { User } from './user/user';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'user', component: User}, 
+    { path: 'user/:id', component: User}, 
+    { path: 'home', component: HomeComponent }];
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf, NgSwitch } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [FormsModule, NgIf, NgFor, NgSwitch],
+  imports: [FormsModule, NgIf, NgFor],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
 export class User {
 
+  id: string | null = null;
+
+  constructor(private router: ActivatedRoute) {
+    this.router.params.subscribe(params => {
+      this.id = params['id'];
+    });
+  }
 
   ngOnInit() {
     console.log('UserComponent initialized');
